f7aa363de590c73d612fedfabc1a44861e333ce0
DefPavel <strikecool1@gmail.com>
2021-07-19 15:44:27 +0300

    add qrcode

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const router = Router();
+const qr = require('qrcode');
 
 // Роутинг на авторизацию
 router.get('/', function(req, res) {
@@ -7,8 +8,25 @@ router.get('/', function(req, res) {
   });
 // Роутинг на главную страницу
   router.get('/home', function(req, res) {
-    res.render('pages/home', { title: 'Главная страница' });
+    res.render('pages/home', { title: 'QR scanner' });
   });
+
+// Роутинг на qrcode
+router.post('/scan', function(req, res) {
+    // Парсим значение из body имеющее name="qrcode"
+    const qrcode_text = req.body.qrcode;
+    
+    if(qrcode_text.length === 0) res.send("Данные пустые");
+    
+    qr.toDataURL(qrcode_text,(err,src) =>{
+    
+    if(err) res.send("Ошибка генерации qrcode");
+    //res.render("scan", {srs} );
+    res.images
+    });
+
+});
+
 
 
 module.exports = router;
